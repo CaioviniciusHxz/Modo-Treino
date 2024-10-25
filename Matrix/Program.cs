588af7308a95605cec392f80849e904459c7cb06
Caio vinicius <caioviniciusdev21@gmail.com>
2024-10-25 06:23:25 -0300

    Codigo do curos, assunto sobre matrix

---
+++ Matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Exercicio do curso
+using System;
+using System.Xml;
+namespace Matrix{
+    class Program{
+        static void Main(string[]args){
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int [n, n];
+            
+            for(int i = 0; i < n; i++){
+                string[] vect = Console.ReadLine().Split(' ');
+                
+                for(int c = 0; c < n; c++) {
+                    mat[i, c] = int.Parse(vect[c]);
+                }
+            }
+            System.Console.WriteLine(" Valores na diagonal");
+            for(int i = 0; i< n; i++){
+                System.Console.Write(mat[i,i] + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
